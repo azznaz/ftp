983867acb4b2540d696753672551af04e9261612
zhouning <zhouning@scutech.com>
2022-09-02 10:06:24 +0800

    fix bug

---
+++ build/CMakeFiles/cserver.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/scutech/azznaz/ftp/Cmd_Handle.cpp" "CMakeFiles/cserver.dir/Cmd_Handle.cpp.o" "gcc" "CMakeFiles/cserver.dir/Cmd_Handle.cpp.o.d"
+  "/home/scutech/azznaz/ftp/Ftp_Server.cpp" "CMakeFiles/cserver.dir/Ftp_Server.cpp.o" "gcc" "CMakeFiles/cserver.dir/Ftp_Server.cpp.o.d"
+  "/home/scutech/azznaz/ftp/server.cpp" "CMakeFiles/cserver.dir/server.cpp.o" "gcc" "CMakeFiles/cserver.dir/server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cserver.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cserver.dir/Cmd_Handle.cpp.o"
+  "CMakeFiles/cserver.dir/Cmd_Handle.cpp.o.d"
+  "CMakeFiles/cserver.dir/Ftp_Server.cpp.o"
+  "CMakeFiles/cserver.dir/Ftp_Server.cpp.o.d"
+  "CMakeFiles/cserver.dir/server.cpp.o"
+  "CMakeFiles/cserver.dir/server.cpp.o.d"
+  "cserver"
+  "cserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
